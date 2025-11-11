1ba401be6af57ff45c145d7941f5baec871bc737
aTrueYety <erikhoff00@gmail.com>
2025-11-11 14:57:43 +0100

    remove depricated prop for keyboard viability

--- app/index.tsx
+++ app/index.tsx
@@ -196,7 +196,6 @@ export default function Index() {
           value={text}
           onChangeText={setText}
           onSubmitEditing={() => addItem()}
-          blurOnSubmit={false}
           returnKeyType="done"
         />
       </View>
